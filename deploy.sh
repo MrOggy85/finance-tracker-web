6c7a9c817e2a739ecffd9785fea1a815c66cbb9c
Oskar Okuno <gitea@okuno.se>
2023-02-16 09:52:37 +0900

    chore: update deply script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
+echo ""
 echo "deploy finance-tracker..."
 
+echo "build..."
+yarn build
+
 echo "remove build folder in server..."
-ssh -T $SSH_HOST -p $SSH_PORT 'rm -rf ${SERVER_FOLDER}/build'
+ssh -T $SSH_HOST -p $SSH_PORT 'rm -rfv ${SERVER_FOLDER}/build'
+echo ""
 
 echo "upload local build folder..."
 scp -P $SSH_PORT -r $(pwd)/build $SSH_HOST:${SERVER_FOLDER}
